30c61f38ad7a05a5a1da5b4a876b8403fbffc3e1
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-04-14 22:31:13 +0100

    Update CategoryRepositoryImpl.kt

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/CategoryRepositoryImpl.kt
@@ -4,8 +4,34 @@ import com.ElOuedUniv.maktaba.data.model.Category
 
 class CategoryRepositoryImpl : CategoryRepository {
 
-    private val categoriesList = listOf<Category>()
-    
+   private val categoriesList = listOf(
+        Category(
+            id = "1",
+            name = "Programming",
+            description = "Books about software development and coding"
+        ),
+        Category(
+            id = "2",
+            name = "Algorithms",
+            description = "Books about algorithms and data structures"
+        ),
+        Category(
+            id = "3",
+            name = "Databases",
+            description = "Books about database design and management"
+        ),
+        Category(
+            id = "2",
+            name = "Mobile Development",
+            description = "Books about Android and mobile application development"
+        ),
+        Category(
+            id = "4",
+            name = "Islamic Studies",
+            description = "Books about Islam, faith and religious guidance"
+        ),
+    )
+
     override fun getAllCategories(): List<Category> {
         TODO("Not yet implemented")
     }
