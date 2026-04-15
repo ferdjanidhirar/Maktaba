514ad5e3d0a95d12d0e12726d0c576fd9f3c8dcf
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-04-15 22:36:51 +0100

    Update GetBooksUseCase.kt

--- app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/GetBooksUseCase.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/GetBooksUseCase.kt
@@ -2,22 +2,13 @@ package com.ElOuedUniv.maktaba.domain.usecase
 
 import com.ElOuedUniv.maktaba.data.model.Book
 import com.ElOuedUniv.maktaba.data.repository.BookRepository
+import javax.inject.Inject
 
-/**
- * Use Case for getting books
- * This follows the Clean Architecture principle of separating business logic
- *
- * Use Cases contain the business logic of the application and are independent
- * of the UI and data sources.
- */
-class GetBooksUseCase(
-    private val bookRepository: BookRepository
+class AddBookUseCase @Inject constructor(
+   
+    private val repository: BookRepository
 ) {
-    /**
-     * Execute the use case to get all books
-     * @return List of all books from the repository
-     */
-    operator fun invoke(): List<Book> {
-        return bookRepository.getAllBooks()
+    suspend operator fun invoke(book: Book) {
+        repository.addBook(book)
     }
 }
