911670125d87c99396e19d86406cdaa4f800efdd
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-04-18 11:47:38 +0100

    Implement AddBookUseCase to add a book

---
+++ app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/AddBookUseCase.kt
@@ -0,0 +1,14 @@
+package com.ElOuedUniv.maktaba.domain.usecase
+
+import com.ElOuedUniv.maktaba.data.model.Book
+import com.ElOuedUniv.maktaba.data.repository.BookRepository
+import javax.inject.Inject
+
+class AddBookUseCase @Inject constructor(
+    private val repository: BookRepository
+) {
+  
+    suspend operator fun invoke(book: Book) {
+        repository.addBook(book)
+    }
+}
