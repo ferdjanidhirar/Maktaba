4ab7b307e3c94a81cdf6740234c9cbd32888b3c5
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-04-28 20:32:03 +0100

    Enhance AddBookUiState with validation and image URI
    Added fields for error messages and form validation.

--- app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/add/AddBookUiState.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/add/AddBookUiState.kt
@@ -1,10 +1,15 @@
 package com.ElOuedUniv.maktaba.presentation.book.add
-
+import android.net.Uri
 data class AddBookUiState(
     val title: String = "",
     val isbn: String = "",
     val nbPages: String = "",
     val isLoading: Boolean = false,
     val isSuccess: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
+    val titleError: String? = null,
+    val isbnError: String? = null,
+    val pagesError: String? = null,
+    val isFormValid: Boolean = false,
+    val imageUri: Uri? = null
 )
