1b9a04931ff434ff0346875d072cd44b7698cb03
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-04-28 21:14:15 +0100

    Update Color.kt

--- app/src/main/java/com/ElOuedUniv/maktaba/presentation/theme/Color.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/presentation/theme/Color.kt
@@ -2,10 +2,16 @@ package com.ElOuedUniv.maktaba.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary Green Palette
+val Green80 = Color(0xFFA5D6A7) 
+val Green60 = Color(0xFF66BB6A)
+val Green40 = Color(0xFF2E7D32) 
+// Dark Green Shades
+val DarkGreen90 = Color(0xFF0A1A0B) 
+val DarkGreen80 = Color(0xFF1B2E1C) 
+val DarkGreen60 = Color(0xFF2D462E) 
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Neutral & Light Shades
+val LightGreen = Color(0xFFE8F5E9) 
+val White = Color(0xFFFFFFFF)      
+val LightGray = Color(0xFFE0E0E0)  
