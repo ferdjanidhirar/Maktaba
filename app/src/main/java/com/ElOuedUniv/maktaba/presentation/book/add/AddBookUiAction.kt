73dbac01cf179ddfd55e558e50c81a9de5095847
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-04-28 20:30:39 +0100

    Update AddBookUiAction.kt

--- app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/add/AddBookUiAction.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/add/AddBookUiAction.kt
@@ -4,5 +4,6 @@ sealed class AddBookUiAction {
     data class OnTitleChange(val title: String) : AddBookUiAction()
     data class OnIsbnChange(val isbn: String) : AddBookUiAction()
     data class OnPagesChange(val pages: String) : AddBookUiAction()
+     data class OnImageChange(val uri: Uri?) : AddBookUiAction()
     object OnAddClick : AddBookUiAction()
 }
