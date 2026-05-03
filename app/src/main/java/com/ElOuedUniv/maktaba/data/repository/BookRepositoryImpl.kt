92a1137fbd281799051d0f547e5622bdd98ce6f4
ferdjanidhirar <ferdjanidhirar@gmail.com>
2026-05-03 21:12:48 +0100

    Update BookRepositoryImpl.kt

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepositoryImpl.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepositoryImpl.kt
@@ -1,38 +1,15 @@
 package com.ElOuedUniv.maktaba.data.repository
 
+import android.net.Uri
 import com.ElOuedUniv.maktaba.data.model.Book
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
-import javax.inject.Inject
-
-class BookRepositoryImpl @Inject constructor() : BookRepository {
-
-    private val _booksList = mutableListOf(
-        Book(isbn = "9780132350884", title = "Clean Code", nbPages = 464, imageUrl = "https://covers.openlibrary.org/b/isbn/9780132350884-L.jpg"),
-        Book(isbn = "9780201616224", title = "The Pragmatic Programmer", nbPages = 352, imageUrl = "https://covers.openlibrary.org/b/isbn/9780201616224-L.jpg"),
-        Book(isbn = "9780201633610", title = "Design Patterns", nbPages = 395, imageUrl = "https://covers.openlibrary.org/b/isbn/9780201633610-L.jpg"),
-        Book(isbn = "9780201485677", title = "Refactoring", nbPages = 461, imageUrl = "https://covers.openlibrary.org/b/isbn/9780201485677-L.jpg"),
-        Book(isbn = "9780596007126", title = "Head First Design Patterns", nbPages = 694, imageUrl = "https://covers.openlibrary.org/b/isbn/9780596007126-L.jpg")
-    )
-
-    private val booksFlow = MutableSharedFlow<List<Book>>(replay = 1).apply {
-        tryEmit(_booksList.toList())
-    }
+interface BookRepository {
     
-    override fun getAllBooks(): Flow<List<Book>> = flow {
-        delay(2000) // Simulate delay
-        emitAll(booksFlow)
-    }
+    fun getAllBooks(): Flow<List<Book>>
+    
+    suspend fun getBookByIsbn(isbn: String): Book?
 
-    override fun getBookByIsbn(isbn: String): Book? {
-        return _booksList.find { it.isbn == isbn }
-    }
+     suspend fun addBook(book: Book, imageUri: Uri?)
 
-    override fun addBook(book: Book) {
-        _booksList.add(book)
-        booksFlow.tryEmit(_booksList.toList())
-    }
+     suspend fun deleteBook(isbn: String)
 }
